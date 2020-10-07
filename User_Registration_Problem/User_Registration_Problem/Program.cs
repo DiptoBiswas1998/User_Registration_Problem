db9c72e0d60f25d3831a2afa23a5b7ecd9a3f5c0
Dipto <diptobiswas1998@gmail.com>
2020-10-07 06:34:31 +0530

    [Dipto] refactor: My first commit

---
+++ User_Registration_Problem/User_Registration_Problem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace User_Registration_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome msg
+            Console.WriteLine("Welcome to the User Registration Problem!");
+        }
+    }
+}
