b250f725f4430a58607a7124291e7abe063d66ae
Dipto Biswas <diptobiswas1998@gmail.com>
2020-10-10 07:38:21 +0530

    [Dipto] refactor: Checking last name also

--- User_Registration_Problem/User_Registration_Problem/Program.cs
+++ User_Registration_Problem/User_Registration_Problem/Program.cs
@@ -8,10 +8,14 @@ namespace User_Registration_Problem
             //Welcome msg
             Console.WriteLine("Welcome to the User Registration Problem!");
             ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
-            Console.WriteLine("Enter your first name: ");
+            Console.WriteLine("Enter your first name : ");
             string firstName = Console.ReadLine();
             bool firstNameResult = validateUserRegistration.ValidateFirstName(firstName);
             validateUserRegistration.PrintResult(firstNameResult);
+            Console.WriteLine("Enter your last name : ");
+            string lastName = Console.ReadLine();
+            bool lastNameResult = validateUserRegistration.ValidateLastName(lastName);
+            validateUserRegistration.PrintResult(lastNameResult);
         }
     }
 }
--- User_Registration_Problem/User_Registration_Problem/ValidateUserRegistration.cs
+++ User_Registration_Problem/User_Registration_Problem/ValidateUserRegistration.cs
@@ -7,19 +7,24 @@ namespace User_Registration_Problem
     class ValidateUserRegistration
     {
         private static readonly string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        private static readonly string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
         public bool ValidateFirstName(string FN)
         {
             return Regex.IsMatch(FN, Regex_FirstName);
+        }
+        public bool ValidateLastName(string LN)
+        {
+            return Regex.IsMatch(LN, Regex_LastName);
         }
         public void PrintResult(bool result)
         {
             if (result)
             {
-                Console.WriteLine("Valid name");
+                Console.WriteLine("Valid name.");
             }
             else
             {
-                Console.WriteLine("Invalid name");
+                Console.WriteLine("Invalid name.");
             }
         }
     }
