e136449f5eebb5f54aadc7d6f52f0278037f9554
Dipto Biswas <diptobiswas1998@gmail.com>
2020-10-10 07:32:47 +0530

    [Dipto] refactor: Checking first name

--- User_Registration_Problem/User_Registration_Problem/Program.cs
+++ User_Registration_Problem/User_Registration_Problem/Program.cs
@@ -7,6 +7,11 @@ namespace User_Registration_Problem
         {
             //Welcome msg
             Console.WriteLine("Welcome to the User Registration Problem!");
+            ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
+            Console.WriteLine("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            bool firstNameResult = validateUserRegistration.ValidateFirstName(firstName);
+            validateUserRegistration.PrintResult(firstNameResult);
         }
     }
 }
