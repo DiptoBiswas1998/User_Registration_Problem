636bd388d553955ed55caeb1fbf5f875e64f776f
Dipto Biswas <diptobiswas1998@gmail.com>
2020-11-04 08:14:33 +0530

    [Dipto] refactor: Using Lambda Function to validate user entry

--- User_Registration_Problem/User_Registration_Testing/UnitTest1.cs
+++ User_Registration_Problem/User_Registration_Testing/UnitTest1.cs
@@ -33,7 +33,7 @@ namespace User_Registration_Testing
         {
             //Arrange
             ValidateUserRegistration user = new ValidateUserRegistration();
-            string email = "dipto.biswas@gmail.com";
+            string email = "diptobiswas.1998@gmail.com";
             //Act
             bool result = user.ValidateEmail(email);
             //Assert
@@ -55,97 +55,11 @@ namespace User_Registration_Testing
         {
             //Arrange
             ValidateUserRegistration user = new ValidateUserRegistration();
-            string password = "Dipto@13579";
+            string password = "Dipto@1998";
             //Act
             bool result = user.ValidatePassword(password);
             //Assert
             Assert.IsTrue(result);
-        }
-        //Sad Test Cases(Test Cases Fail The Entry)
-        [TestMethod]
-        public void GivenUserFistName_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_FirstName()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string firstName = "Dipto";
-                //Act
-                bool result = user.ValidateFirstName(firstName);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid first name.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserLastName_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_LastName()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string lastName = "Biswas";
-                //Act
-                bool result = user.ValidateLastName(lastName);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid last name.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserEmail_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_Email()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string email = "dipto.biswas.gmail.com";
-                //Act
-                bool result = user.ValidateEmail(email);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid email address.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserMobileNumber_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_MobileNo()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string mobileNumber = "11 0123456789";
-                //Act
-                bool result = user.ValidateMobileNo(mobileNumber);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid mobile number.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserPassword_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_Password()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string password = "Dipto@13579";
-                //Act
-                bool result = user.ValidatePassword(password);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid password.", e.Message);
-            }
         }
         [TestMethod]
         [DataRow("abc@yahoo.com")]
